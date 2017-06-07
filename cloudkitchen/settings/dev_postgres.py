7001b3afe8fe6733f8943e2d191c4a024c9f3c63
Ramsés Martinez <ramses.mtz96@gmail.com>
2017-06-07 01:57:49 -0500

    The return of the beast

---
+++ cloudkitchen/settings/dev_postgres.py
@@ -0,0 +1,12 @@
+from cloudkitchen.settings.devp import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('DABBANET_DB_NAME'),
+        'USER': os.getenv('DABBANET_DB_USER'),
+        'PASSWORD': os.getenv('DABBANET_DB_PASSWORD'),
+        'HOST': os.getenv('DABBANET_DB_HOST'),
+        'PORT': os.getenv('DABBANET_DB_PORT'),
+    }
+}
