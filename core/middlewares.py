4e3b2c2d26cd1dbb943e3a4722fee6304f841b6c
Ramsés Martinez <ramses.mtz96@gmail.com>
2017-06-18 13:15:47 -0500

    Changed middleware to detet the correct host

--- core/middlewares.py
+++ core/middlewares.py
@@ -8,8 +8,8 @@ class HostMiddleware(MiddlewareMixin):
         try:
             host = request.META['HTTP_HOST'] + request.META['PATH_INFO']
             print(host)
-            # if host == 'dabbawala.com.mx/' or host == 'www.dabbawala.com.mx/':
-            # return redirect('users:index')
+            if host == 'dabbawala.com.mx' or host == 'www.dabbawala.com.mx':
+                return redirect('users:index')
 
         except KeyError:
             return None
