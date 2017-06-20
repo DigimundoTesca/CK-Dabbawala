0671c82654b1498b49d527b9c6f0fc4ba3ed5bea
Ramsés Martinez <ramses.mtz96@gmail.com>
2017-06-20 00:53:34 -0500

    Captured the first properties of packages

--- cloudkitchen/static/js/orders/scripts.js
+++ cloudkitchen/static/js/orders/scripts.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
 
+  Ticket = {
+    packages: [],
+    cartridges: []
+  }
+
   function getCookie(name) {
     let cookieValue = null;
     if (document.cookie && document.cookie !== '') {
@@ -7,16 +12,23 @@ $(document).ready(function() {
       let i = 0;
       for (; i < cookies.length; i++) {
         let cookie = jQuery.trim(cookies[i]);
-          // Does this cookie string begin with the name we want?
-          if (cookie.substring(0, name.length + 1) === (name + '=')) {
-            cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-            break;
-          }
+        /** Does this cookie string begin with the name we want? */
+        if (cookie.substring(0, name.length + 1) === (name + '=')) {
+          cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+          break;
         }
       }
-      return cookieValue;
     }
+    return cookieValue;
+  }
+
+  $('.product').click(function(){
+    let productId = $(this).attr('id').split('-')[1];
+    let productName = $(this).find('.product-name').text();
+    let productCost = $(this).find('.product-cost').text();
     
+  });
+  
 });
 
   function visible() {
