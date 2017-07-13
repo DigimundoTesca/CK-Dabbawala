3b569cbe5d72104a74c318dfc3398e0be9f3d3d8
Ramsés Martinez <ramses.mtz96@gmail.com>
2017-07-12 23:12:47 -0500

    Changed User Model with Roles

--- users/admin.py
+++ users/admin.py
@@ -1,21 +1,16 @@
 from django.contrib import admin
 
-from users.models import User as UserProfile, Rol, UserMovements
+from users.models import User as UserProfile, UserMovements
 
 
 class UserProfileAdmin(admin.ModelAdmin):
 	pass
 
 
-class RolAdmin(admin.ModelAdmin):
-    pass
-
-
 @admin.register(UserMovements)
 class UserMovements(admin.ModelAdmin):
-    list_display = ('user', 'category', 'creation_date',)    
+    list_display = ('user', 'category', 'creation_date',)
     ordering = ('creation_date',)
 
 
 admin.site.register(UserProfile, UserProfileAdmin)
-admin.site.register(Rol, RolAdmin)
--- users/models.py
+++ users/models.py
@@ -7,25 +7,33 @@ from django.db import models
 from django.utils import six
 
 
-class Rol(models.Model):
-    rol = models.CharField(max_length=90, default='')
-
-    class Meta:
-        verbose_name = 'Rol'
-        verbose_name_plural = 'Roles'
-
-    def __str__(self):
-        return '%s' % self.rol
-
-
 class User(AbstractUser):
-    user_rol = models.ForeignKey(Rol, blank=True, null=True)
+    # User Rol
+    ADMIN = 'AD'
+    CEO = 'CE'
+    MANAGER = 'MA'
+    CHEF = 'CH'
+    STORER = 'ST'
+    DELIVERY_MAN = 'DM'
+    CUSTOMER = 'CU'
+
+    USER_ROL = (
+        (ADMIN, 'Admin'),
+        (STORER, 'Almacenista'),
+        (CEO, 'CEO'),
+        (CHEF, 'Chef'),
+        (CUSTOMER, 'Cliente'),
+        (MANAGER, 'Gerente'),
+        (DELIVERY_MAN, 'Repartidor'),
+    )
+
+    user_rol = models.CharField(choices=USER_ROL, default=CUSTOMER, max_length=2)
 
     class Meta(AbstractUser.Meta):
         swappable = 'AUTH_USER_MODEL'
 
 
-class UserMovements(models.Model):        
+class UserMovements(models.Model):
     user = models.CharField(max_length=20,default='None')
     category = models.CharField(max_length=20,default='None')
     creation_date = models.DateField(auto_now=True)
