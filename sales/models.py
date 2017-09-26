cb5d5fc2ace339472f7beb42759e483aec2d2310
Jonathan Basilio <jonathanbasilio24@outlook.com>
2017-09-26 02:39:13 -0500

    delete remove cartridge sales model

--- sales/models.py
+++ sales/models.py
@@ -197,17 +197,3 @@ class TicketExtraIngredient(models.Model):
 
     def __str__(self):
         return '%s' % self.extra_ingredient
-
-
-class SalesCartridge(models.Model):
-    cartridge = models.ForeignKey(Cartridge, on_delete=models.CASCADE)
-    quantity = models.PositiveSmallIntegerField(default=0)
-    created_at = models.DateTimeField(editable=True)
-    simulated_quantity = models.PositiveSmallIntegerField(null=True)
-
-    class Meta:
-        verbose_name = 'Venta por dia de cartucho'
-        verbose_name_plural = 'Ventas por dia de cartuchos'
-
-    def __str__(self):
-        return '%s' % self.cartridge
