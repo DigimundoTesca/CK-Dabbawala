05380250562db049eff2799e7093fd54a2c78c18
Ramsés Martinez <ramses.mtz96@gmail.com>
2018-02-21 13:51:29 -0600

    chore: Add gulp-sass task for development environment

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,18 @@
-gulpfile.js
+const gulp = require('gulp'), 
+  sass = require('gulp-sass'),
+  autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('sass', ()=>
+  gulp.src('./cloudkitchen/static/scss/*scss')
+  .pipe(sass({
+    outputStyle: 'compressed'
+  }))
+  .pipe(autoprefixer({
+    versions: ['last 5 browsers']
+  }))
+  .pipe(gulp.dest('./cloudkitchen/static/css'))
+  );
+
+gulp.task('default', () => {
+  gulp.watch('./cloudkitchen/static/scss/*.scss', ['sass'])
+})
