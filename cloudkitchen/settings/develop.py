669ed268b626dd6a176cd43240f047754ef0657a
Ramses Martinez <ramses.mtz96@gmail.com>
2018-03-07 19:46:33 -0600

    refactor: Change dirs path code and add webpack-stats.json to gitignore

--- cloudkitchen/settings/develop.py
+++ cloudkitchen/settings/develop.py
@@ -5,7 +5,7 @@ ALLOWED_HOSTS = ['*']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, '../db_dabbanet.sqlite3'),
+        'NAME': os.path.join(os.path.dirname(BASE_DIR), 'db_dabbanet.sqlite3'),
     }
 }
 
