a175dc664e597a6b74df63c4da14cd9df90ed244
Ramses Martinez <ramses.mtz96@gmail.com>
2018-03-19 02:24:39 -0600

    fix: Refactor and fix activate classs in header nav & refactor app.js

--- src/js/app.js
+++ src/js/app.js
@@ -4,42 +4,66 @@ const PATH = window.location.pathname;
  *
  */
 export function loadPathname() {
-  if (PATH === '/ventas/') {
-    let linkElement = document.getElementById('link-sales');
-    $('#link-sales').addClass('active');
-  } else if (PATH === '/sales/new/breakfast/') {
-    $('#link-new-breakfast').addClass('active');
-  } else if (PATH === '/sales/new/food/') {
-    $('#link-new-food').addClass('active');
-  } else if (PATH === '/supplies/' || PATH === '/supplies/new/') {
-    $('#link-warehouse').addClass('active');
-  } else if (PATH === '/cartridges/' || PATH === '/cartridges/new/') {
-    $('#link-warehouse').addClass('active');
-  } else if (PATH === '/customers/register/list/') {
-    $('#link-customers').addClass('active');
-  } else if (PATH === '/kitchen/' || PATH === '/kitchen/assembly/') {
-    $('#link-kitchen').addClass('active');
-  } else if (PATH === '/diners/' || PATH === '/diners/logs/') {
-    $('#link-diners').addClass('active');
-  }
+    let el;
+    switch (PATH) {
+        case '/ventas/':
+        case '/ventas/nueva-venta/':
+            el = document.getElementById('link-sales');
+            break;
+        case '/cocina/friad/':
+        case '/cocina/caliente/':
+        case '/cocina/ensamblado/':
+            el = document.getElementById('link-kitchen');
+            break;
+        case '/warehouse/':
+        case '/warehouse/analytics/':
+        case '/warehouse/shoplist':
+        case '/warehouse/new_shoplist':
+        case '/supplies/':
+        case '/cartridges/':
+            el = document.getElementById('link-warehouse');
+            break;
+        case '/diners/':
+        case '/diners/log/':
+            el = document.getElementById('link-dinning-room');
+            break;
+        case '/diners/analytics/':
+        case '/diners/suggestions/':
+            el = document.getElementById('link-diners');
+            break;
+        case '/settings/':
+            el = document.getElementById('link-panel');
+            break;
+    }
+
+    addClass(el, 'active');
+
+    function addClass(el, className) {
+        if (el.classList)
+            el.classList.add(className);
+        else
+            el.className += ' ' + className;
+    }
 }
 
 /**
  * Obtención de Cookies
- * 
+ *
  */
 export function getCookie(name) {
-  let cookieValue = null;
-  if (document.cookie && document.cookie !== '') {
-    let cookies = document.cookie.split(';');
-    for (let i = 0; i < cookies.length; i++) {
-      let cookie = jQuery.trim(cookies[i]);
-      // Does this cookie string begin with the name we want?
-      if (cookie.substring(0, name.length + 1) === (name + '=')) {
-        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-        break;
-      }
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        let cookies = document
+            .cookie
+            .split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            let cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
     }
-  }
-  return cookieValue;
+    return cookieValue;
 }
