56927d875db8f9d4dc1b90b9dc2782c69096d452
Ramsés Martínez Ortiz <ramses.mtz96@gmail.com>
2018-05-13 00:07:00 -0500

    feature: Finish ticket sales design
    - Add new fonts to default_vfs.js file

--- src/js/app.js
+++ src/js/app.js
@@ -87,3 +87,31 @@ export function findParentBySelector(elm, selector) {
   }
   return cur; //will return null if not found
 }
+
+/**
+ * Permite dar formato con decimales a un numero
+ * @param amount
+ * @param decimals
+ * @returns {string}
+ */
+export function numberFormat(amount, decimals) {
+    amount += ''; // por si pasan un numero en vez de un string
+    amount = parseFloat(amount.replace(/[^0-9\.]/g, '')); // elimino cualquier cosa que no sea numero o punto
+
+    decimals = decimals || 0; // por si la variable no fue fue pasada
+
+    // si no es un numero o es igual a cero retorno el mismo cero
+    if (isNaN(amount) || amount === 0)
+        return parseFloat(0).toFixed(decimals);
+
+    // si es mayor o menor que cero retorno el valor formateado como numero
+    amount = '' + amount.toFixed(decimals);
+
+    let amount_parts = amount.split('.'),
+        regexp = /(\d+)(\d{3})/;
+
+    while (regexp.test(amount_parts[0]))
+        amount_parts[0] = amount_parts[0].replace(regexp, '$1' + ',' + '$2');
+
+    return amount_parts.join('.');
+}
